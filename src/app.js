29dfe52a304e585706f556e30bd66f1baccfe039
mehmet ali <alimehmet1@windowslive.com>
2021-10-22 11:50:02 +0300

    jscamp

---
+++ src/app.js
@@ -0,0 +1,24 @@
+console.log("Merhaba Dünya");
+
+let dolarBugun=9.30
+
+let dolarDun=9.20
+
+{
+    let dolarDun=9.10
+}
+
+console.log(dolarDun)
+
+const eurodun=11.2
+
+console.log(eurodun)
+
+
+let Krediler=["ihtiyaç","taşıt","konuk"]
+console.log("<ul>")
+for (let i = 0; i < Krediler.length; i++) {
+    console.log("<li>"+Krediler[i]+"</li>")   
+}
+console.log("<ul>")
+console.log(Krediler)
